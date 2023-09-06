e959f3155c02f8a6d89b420fe43caba597d0fd6c
Mihail <mikhail.perov.a@gmail.com>
2023-09-06 15:56:39 +0300

    Add display students by major

--- main.cpp
+++ main.cpp
@@ -36,31 +36,51 @@ void displayStudents(const std::vector<Student>& database) {
     }
 }
 
+// Функция для вывода списка студентов по названию специальности.
+void displayStudentsByMajor(const std::vector<Student>& database, std::string major) {
+    for (const Student& student : database) {
+        if (student.major != major) {
+            continue;
+        }
+        std::cout << "Имя: " << student.name << "\n";
+        std::cout << "Возраст: " << student.age << "\n";
+        std::cout << "Специальность: " << student.major << "\n";
+        std::cout << "Средний балл: " << student.gpa << "\n\n";
+    }
+}
+
 int main() {
     std::setlocale(LC_ALL, "rus");
     std::vector<Student> database;
 
     int choice;
+    std::string major;
     do {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Вывести список студентов по специальности\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
 
         switch (choice) {
-            case 1:
-                addStudent(database);
-                break;
-            case 2:
-                displayStudents(database);
-                break;
-            case 0:
-                std::cout << "Выход из программы.\n";
-                break;
-            default:
-                std::cout << "Неверный выбор. Попробуйте снова.\n";
+        case 1:
+            addStudent(database);
+            break;
+        case 2:
+            displayStudents(database);
+            break;
+        case 3:
+            std::cout << "Выберите специальность: ";
+            std::cin >> major;
+            displayStudentsByMajor(database, major);
+            break;
+        case 0:
+            std::cout << "Выход из программы.\n";
+            break;
+        default:
+            std::cout << "Неверный выбор. Попробуйте снова.\n";
         }
     } while (choice != 0);
 
